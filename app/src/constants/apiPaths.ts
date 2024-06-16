2847e9f78adfebc9b2667c874616901fd7668a7f
Tamar <Tamar_Ratiani@epam.com>
2024-06-17 03:03:45 +0400

    init react app

---
+++ app/src/constants/apiPaths.ts
@@ -0,0 +1,3 @@
+export const API_PATHS = {
+    product: 'https://mu2y5qflf2.execute-api.eu-central-1.amazonaws.com/prod'
+}
---
+++ app/src/models/Product.ts
@@ -0,0 +1,6 @@
+export interface AvailableProduct {
+    id: string;
+    name: string;
+    price: number;
+    imageUrl: string;
+  }
