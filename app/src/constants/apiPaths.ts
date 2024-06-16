a809a5af38d17640ffb8dd10bdcef9f1496389fd
Tamarchikk <Tamar_Ratiani@epam.com>
2024-06-17 03:10:00 +0400

    Delete app directory

--- app/src/constants/apiPaths.ts
+++
@@ -1,3 +0,0 @@
-export const API_PATHS = {
-    product: 'https://mu2y5qflf2.execute-api.eu-central-1.amazonaws.com/prod'
-}
--- app/src/models/Product.ts
+++
@@ -1,6 +0,0 @@
-export interface AvailableProduct {
-    id: string;
-    name: string;
-    price: number;
-    imageUrl: string;
-  }
