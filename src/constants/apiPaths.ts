72245bcdb25345766c5db689a7fb899fa0f4bcd1
Tamar <Tamar_Ratiani@epam.com>
2024-06-24 14:04:39 +0400

    update api_paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://mu2y5qflf2.execute-api.eu-central-1.amazonaws.com/prod",
+  order: "https://mu2y5qflf2.execute-api.eu-central-1.amazonaws.com/prod",
+  import: "https://mu2y5qflf2.execute-api.eu-central-1.amazonaws.com/prod",
+  bff: "https://mu2y5qflf2.execute-api.eu-central-1.amazonaws.com/prod",
+  cart: "https://mu2y5qflf2.execute-api.eu-central-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
